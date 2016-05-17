4b1273b9080daf490f7de90a423a5cb0fc53e74f
Jifeng Zhang <zjfroot@gmail.com>
2016-05-17 17:01:50 +0200

    Added some comments and changed app name

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker stop hello-production
-docker run --rm -p 8000:80 --name hello-production hello-world
+docker stop go-app-production
+docker run --rm -p 8000:80 --name go-app-production go-app
