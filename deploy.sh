339a17c60e5d26a480c03d2a213adb7cb6989d0c
Jifeng Zhang <zjfroot@gmail.com>
2016-05-17 17:38:03 +0200

    added detached mode script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 docker stop go-app-production
 docker run --rm -p 8000:80 --name go-app-production go-app
+#run in detached mode:
+#docker run -d -p 8000:80 --name go-app-production zjfroot/go-app:1.0.0
